8f467354aedbed1f1d65e6647d5b8fd0fb6310c3
Alex Chythlook <achythlook@gmail.com>
2018-02-26 18:07:21 -0900

    Change version of Clojure to 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]]
